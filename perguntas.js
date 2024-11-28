2aa1847fa420be1068227f8983d521ebb0c94e77
barbieri0-0 <00001111304142sp@al.educacao.sp.gov.br>
2024-11-28 11:25:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Verity',
+    'O que é "Verity" e qual é o tema principal?',
+    '"Verity" é um suspense psicológico sobre Lowen, uma escritora que descobre segredos sombrios de Verity, uma autora famosa. O tema gira em torno da manipulação, confiança e a linha tênue entre ficção e realidade.'
+)
+
+criaCartao(
+    'Até o Verão Terminar',
+    'Como se desenvolve a história de "Até o verão terminar"?',
+    '"Até o verão terminar" segue Auden, que passa o verão com seu pai e conhece Eli. O romance lida com amadurecimento, autoconhecimento e os desafios dos relacionamentos.'
+)
+
+criaCartao(
+    'Mil Beijos de Um Garoto',
+    'O que torna "Mil beijos de um garoto" emocionalmente intenso?',
+    'A história de Poppy e Rune é profunda, explorando temas de amor, perda e superação. Eles se apaixonam na infância, se separam, e se reencontram anos depois, lidando com segredos e dificuldades.'
+)
+
+criaCartao(
+    'A Bibliteca da Meia Noite',
+    'Qual é a mensagem de "A Biblioteca da Meia-Noite"?',
+    'O livro mostra Nora explorando diferentes versões de sua vida em uma biblioteca mágica. A mensagem principal é sobre aceitar as escolhas feitas, encontrar felicidade nas imperfeições e valorizar a vida como ela é.'
+)
